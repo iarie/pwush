ebd1bd02a83dc554ca87b2f85ee8cce7d021b5c2
Yaroslav Litvinov <beyondthemkad@gmail.com>
2018-09-10 14:07:03 +0300

    add device-api methods (#4)

--- lib/pwush/client.rb
+++ lib/pwush/client.rb
@@ -35,6 +35,44 @@ module Pwush
       post(:getPreset, preset_code: preset_code)
     end
 
+    ### DEVICE API
+
+    def register_device(params)
+      post(:registerDevice, params)
+    end
+
+    def unregister_device(hwid)
+      post(:unregisterDevice, hwid: hwid)
+    end
+
+    def create_test_device(params)
+      post(:createTestDevice, params)
+    end
+
+    def list_test_devices
+      post(:listTestDevices)
+    end
+
+    def set_badge(hwid, badge)
+      post(:setBadge, hwid: hwid, badge: badge)
+    end
+
+    def application_open(hwid)
+      post(:applicationOpen, hwid: hwid)
+    end
+
+    def push_stat(hwid, hash_tag = nil)
+      post(:pushStat, hwid: hwid, hash: hash_tag)
+    end
+
+    def message_delivery_event(hwid, hash_tag = nil)
+      post(:messageDeliveryEvent, hwid: hwid, hash: hash_tag)
+    end
+
+    def set_purchase(hwid, params = {})
+      post(:setPurchase, params.merge(hwid: hwid))
+    end
+
     private
 
     def get(action, payload = nil)
