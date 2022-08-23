21e26e5f397a6ef5332c14f7170bde61ca68f2f2
Yaroslav Litvinov <beyondthemkad@gmail.com>
2022-08-23 12:44:41 +0400

    remove deprecated Dry::Struct::Value

--- lib/pwush/response/value.rb
+++ lib/pwush/response/value.rb
@@ -1,6 +1,6 @@
 module Pwush
   module Response
-    class Value < Dry::Struct::Value
+    class Value < Dry::Struct
       attribute :status_code, Types::Coercible::Integer
       attribute? :status_message, Types::Strict::String
       attribute? :body, (Types::Hash | Types::String).optional
